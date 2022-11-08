ce0768bd071e5feee5d22644b0e6aad5726d8ac8
NajmeShirazi <117516644+NajmeShirazi@users.noreply.github.com>
2022-11-08 12:01:24 +0330

    KNN Model

--- KNN- SHHS1.py
+++ KNN- SHHS1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[183]:
+# In[73]:
 
 
 # Importing the required packages
@@ -9,55 +9,46 @@ import numpy as np
 import pandas as pd
 
 
-# In[184]:
+# In[74]:
 
 
 # Importing Dataset
-dataPatient = pd.read_excel(r'C:\Users\Najmeh\Desktop\New Patient\patients-15feature.xlsx')
-dataNormal = pd.read_excel(r'C:\Users\Najmeh\Desktop\New Patient\Absolutely Normal- 15 Feature.xlsx')
+dataPatient = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Patients- Selected Features1.xlsx')
+dataNormal = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Absolutely Normal- Selected Features1.xlsx')
 X = np.concatenate(  (dataPatient,dataNormal),axis=0  )
 
 y = [0]*len(dataNormal)+[1]*len(dataPatient)
 
 
-# In[185]:
+# In[75]:
 
 
 len(X)
 
 
-# In[186]:
+# In[76]:
 
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
 
-# In[187]:
+# In[77]:
 
 
 # Splitting the dataset into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 1)
 
 
-# In[188]:
+# In[78]:
 
 
-knn = KNeighborsClassifier(n_neighbors=7, metric='euclidean') 
+knn = KNeighborsClassifier(n_neighbors = 5, metric='euclidean') 
 # grid search hyper parametre (in sklearn)
 knn.fit(X_train, y_train)
 
 
-# In[189]:
-
-
-
-from sklearn.ensemble import RandomForestClassifier
-rf_model = RandomForestClassifier(n_estimators=50, max_features="auto", random_state=44)
-rf_model.fit(X_train, y_train)
-
-
-# In[190]:
+# In[79]:
 
 
 # Function to calculate accuracy
@@ -76,34 +67,9 @@ def cal_accuracy(y_test, y_pred):
     classification_report(y_test, y_pred))
 
 
-# In[191]:
+# In[80]:
 
 
 y_pred = knn.predict(X_test)
 cal_accuracy(y_test, y_pred)
-
-
-# In[192]:
-
-
-y_pred = rf_model.predict(X_test)
-cal_accuracy(y_test, y_pred)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
