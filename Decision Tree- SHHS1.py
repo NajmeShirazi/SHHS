f094dc6e3ac10a618c755a70260c73292aa85742
NajmeShirazi <117516644+NajmeShirazi@users.noreply.github.com>
2022-11-24 21:44:07 +0330

    Decision Tree Model to Distinguish Healthy and Sick People

--- Decision Tree- SHHS1.py
+++ Decision Tree- SHHS1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[22]:
 
 
 # Importing the required packages
@@ -10,75 +10,75 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# In[2]:
+# In[23]:
 
 
 from sklearn import tree
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 
-# In[3]:
+# In[24]:
 
 
 # Importing Dataset
-dataPatient = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Patients- Selected Features1.xlsx')
-dataNormal = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Absolutely Normal- Selected Features1.xlsx')
-X = np.concatenate(  (dataPatient,dataNormal),axis=0  )
+dataPatient = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Stroke\Patients- Stroke- SF1.xlsx')
+dataNormal = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Stroke\Absolutely Normal- SF1.xlsx')
+X = np.concatenate(  (dataPatient,dataNormal), axis=0  )
 
 y = [0]*len(dataNormal)+[1]*len(dataPatient)
 
 
-# In[4]:
+# In[25]:
 
 
 # Splitting the dataset into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 100)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 1)
 
 
-# In[5]:
+# In[26]:
 
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
+clf = clf.fit(X_train, y_train)
 
 
-# In[6]:
+# In[27]:
 
 
 tree.plot_tree(clf)
 [...]
 
 
-# In[7]:
+# In[36]:
 
 
 # Decision tree with entropy
-clf_entropy = DecisionTreeClassifier(criterion='entropy', max_depth = 5, min_samples_leaf = 5, random_state = 100)
+clf_entropy = DecisionTreeClassifier(criterion='entropy', max_depth = 3, min_samples_leaf = 3, random_state = 100)
 
 
-# In[8]:
+# In[37]:
 
 
 # Performing training
 clf_entropy.fit(X_train, y_train)
 
 
-# In[9]:
+# In[38]:
 
 
 # Creating the classifier object
-clf_gini = DecisionTreeClassifier(criterion = "gini", max_depth= 5, min_samples_leaf=5, random_state = 100)
+clf_gini = DecisionTreeClassifier(criterion = "gini", max_depth= 3, min_samples_leaf= 3, random_state = 100)
 
 
-# In[10]:
+# In[39]:
 
 
 # Performing training
 clf_gini.fit(X_train, y_train)
 
 
-# In[11]:
+# In[40]:
 
 
 # Function to calculate accuracy
@@ -97,26 +97,35 @@ def cal_accuracy(y_test, y_pred):
     classification_report(y_test, y_pred))
 
 
-# In[12]:
+# In[41]:
+
+
+y_pred=clf.predict(X_test)
+cal_accuracy(y_test, y_pred)
+
+
+# In[42]:
 
 
 y_pred=clf_entropy.predict(X_test)
-
-
-# In[13]:
-
-
 cal_accuracy(y_test, y_pred)
 
 
-# In[14]:
+# In[43]:
 
 
 y_pred=clf_gini.predict(X_test)
-
-
-# In[15]:
-
-
 cal_accuracy(y_test, y_pred)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
