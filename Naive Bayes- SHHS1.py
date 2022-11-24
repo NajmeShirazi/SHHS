5b15b50f3f59bcb59efcc54803ecb5c5698f69d8
NajmeShirazi <117516644+NajmeShirazi@users.noreply.github.com>
2022-11-24 22:36:21 +0330

    Naive Bayes Model to Distinguish Healthy and Sick People

--- Naive Bayes- SHHS1.py
+++ Naive Bayes- SHHS1.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 
 
 # Importing Dataset
-dataPatient = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\All Patients\Patients-Str-HrtVsl-HrtFlr.xlsx')
-dataNormal = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\All Patients\Absolutely Normal- SF1.xlsx')
+dataPatient = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Stroke\Patients- Stroke- SF1.xlsx')
+dataNormal = pd.read_excel(r'C:\Users\Najmeh\Desktop\New\Stroke\Absolutely Normal- SF1.xlsx')
 X = np.concatenate(  (dataPatient,dataNormal), axis=0  )
 
 y = [0]*len(dataNormal)+[1]*len(dataPatient)
@@ -32,7 +32,7 @@ len(X)
 from sklearn.model_selection import train_test_split
 
 
-# In[5]:
+# In[8]:
 
 
 # Splitting the dataset into train and test
@@ -40,18 +40,17 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 1)
 
 
-# In[6]:
+# In[9]:
 
 
 from sklearn.naive_bayes import BernoulliNB
 
-model = BernoulliNB().fit(X, y)
-
-model.fit(X, y)
+model = BernoulliNB().fit(X_train, y_train)
+model.fit(X_train, y_train)
 predicted_signal = model.predict(X_test)
 
 
-# In[7]:
+# In[10]:
 
 
 # Function to calculate accuracy
@@ -70,17 +69,11 @@ def cal_accuracy(y_test, y_pred):
     classification_report(y_test, y_pred))
 
 
-# In[8]:
+# In[11]:
 
 
 y_pred = model.predict(X_test)
 cal_accuracy(y_test, y_pred)
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
